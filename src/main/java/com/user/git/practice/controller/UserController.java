0d08d49e4a685eacb9cd0b29ee847b422458d628
Kunal <kunalkumar3191@gmail.com>
2021-01-24 14:51:10 -0500

    added code for getAllUser.

--- src/main/java/com/user/git/practice/controller/UserController.java
+++ src/main/java/com/user/git/practice/controller/UserController.java
@@ -1,4 +1,21 @@
 package com.user.git.practice.controller;
 
+import com.user.git.practice.model.User;
+import com.user.git.practice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/git")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    public List<User> getUserList(){
+        return userService.getUserList();
+    }
 }
